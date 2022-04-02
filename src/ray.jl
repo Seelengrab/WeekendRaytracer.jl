2d01f4500861e9e158d0305f09483125e6e9ce55
Sukera <Seelengrab@users.noreply.github.com>
2022-04-03 00:02:03 +0200

    Fix utility function bugs & be more accepting in terms of numbers

--- src/ray.jl
+++ src/ray.jl
@@ -7,4 +7,4 @@ ray() = ray(point3(0,0,0), vec3(0,0,0))
 origin(r::ray) = r.orig
 direction(r::ray) = r.dir
 
-at(r::ray, t::Float64) = r.dir + t * r.dir
+at(r::ray, t::Real) = r.dir + t * r.dir
--- src/vec3.jl
+++ src/vec3.jl
@@ -7,18 +7,18 @@ end
 Base.:-(v::vec3) = vec3(-v.x, -v.y, -v.z)
 Base.getindex(v::vec3, i::Int) = getfield(v, i)
 Base.:+(this::vec3, v::vec3) = vec3(this.x + v.x, this.y + v.y, this.z + v.z)
-Base.:*(this::vec3, t::Float64) = vec3(this.x * t, this.y * t, this.z * t)
-Base.:*(t::Float64, v::vec3) = v*t
-Base.:/(this::vec3, t::Float64) = vec3 * 1.0/t
-Base.:/(t::Float64, v::vec3) = 1.0/t * vec3
+Base.:*(this::vec3, t::Real) = vec3(this.x * t, this.y * t, this.z * t)
+Base.:*(t::Real, v::vec3) = v*t
+Base.:/(v::vec3, t::Real) = v * inv(t)
+Base.:/(t::Real, v::vec3) = inv(t) * v
 Base.length(v::vec3) = sqrt(length²(v))
 length²(v::vec3) = v.x*v.x + v.y*v.y + v.z*v.z
 
 const point3 = vec3
 const color = vec3
 
-Base.:-(v::vec3, u::vec3) = vec3(v.x - v.x, v.y - v.y, v.z - v.z)
-Base.:*(v::vec3, u::vec3) = vec3(v.x * v.x, v.y * v.y, v.z * v.z)
+Base.:-(v::vec3, u::vec3) = vec3(v.x - u.x, v.y - u.y, v.z - u.z)
+Base.:*(v::vec3, u::vec3) = vec3(v.x * u.x, v.y * u.y, v.z * u.z)
 
 function dot(u::vec3, v::vec3)
     return u.x * v.x +
