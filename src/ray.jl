e10a957874140a0e8926f190c681dbb6fc875164
Sukera <Seelengrab@users.noreply.github.com>
2022-04-07 09:18:01 +0200

    Add time of ray

--- src/ray.jl
+++ src/ray.jl
@@ -1,10 +1,13 @@
 struct ray
     orig::point3
     dir::vec3
+    tm::Float64
 end
-ray() = ray(point3(0,0,0), vec3(0,0,0))
+ray(orig::point3, dir::vec3) = ray(orig, dir, 0.0)
+ray() = ray(point3(0,0,0), vec3(0,0,0), 0.0)
 
 origin(r::ray) = r.orig
 direction(r::ray) = r.dir
+time(r::ray) = r.tm
 
 at(r::ray, t::Real) = r.orig + t * r.dir
