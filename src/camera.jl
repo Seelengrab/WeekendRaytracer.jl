0f28575818098f49f3b4986e211f25e42b83a45f
Sukera <Seelengrab@users.noreply.github.com>
2022-04-07 09:26:04 +0200

    Modify camera to have shutter time

--- src/camera.jl
+++ src/camera.jl
@@ -7,9 +7,12 @@ struct camera
     v::vec3
     w::vec3
     lens_radius::Float64
+    # shutter open/close time
+    _time0::Float64
+    _time1::Float64
 end
 
-function camera(lookfrom::point3, lookat::point3, vup::vec3, vfov::Float64, aspect_ratio::Float64, aperture::Float64, focus_dist::Float64)
+function camera(lookfrom::point3, lookat::point3, vup::vec3, vfov::Float64, aspect_ratio::Float64, aperture::Float64, focus_dist::Float64, _time0::Float64, _time1::Float64)
     theta = deg2rad(vfov)
     h = tan(theta/2)
     viewport_height = 2.0 * h
@@ -24,12 +27,12 @@ function camera(lookfrom::point3, lookat::point3, vup::vec3, vfov::Float64, aspe
     vertical = focus_dist * viewport_height * v
     lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w
 
-    camera(origin, lower_left_corner, horizontal, vertical, u,v,w,aperture/2)
+    camera(origin, lower_left_corner, horizontal, vertical, u,v,w,aperture/2, _time0, _time1)
 end
 
 function get_ray(c::camera, s::Float64, t::Float64)
     rd = c.lens_radius * rand(InUnitDisk())
     offset = c.u * rd.x + c.v * rd.y
     return ray(c.origin + offset,
-               c.lower_left_corner + s*c.horizontal + t*c.vertical - c.origin - offset)
+               c.lower_left_corner + s*c.horizontal + t*c.vertical - c.origin - offset, rand(BoundedFloat64(c._time0, c._time1)))
 end
