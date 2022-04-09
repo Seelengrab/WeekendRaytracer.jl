450a9bd72582b8f81a1a437b6c7e9c5c9b64f63c
Sukera <Seelengrab@users.noreply.github.com>
2022-04-09 15:53:06 +0200

    Make printing faster by eliding error checking

--- src/color.jl
+++ src/color.jl
@@ -4,11 +4,11 @@ function write_color(io::IO, pixel_color::color, samples_per_pixel::Int)
     b = pixel_color.z
 
     scale = 1.0 / samples_per_pixel
-    r = sqrt(scale * r)
-    g = sqrt(scale * g)
-    b = sqrt(scale * b)
+    r = sqrt(scale * r) * !isnan(r)
+    g = sqrt(scale * g) * !isnan(g)
+    b = sqrt(scale * b) * !isnan(b)
 
-    print(io, trunc(Int, 256 * clamp(r, 0.0, 0.999)), ' ',
-              trunc(Int, 256 * clamp(g, 0.0, 0.999)), ' ',
-              trunc(Int, 256 * clamp(b, 0.0, 0.999)), '\n')
+    print(io, unsafe_trunc(Int, 256 * clamp(r, 0.0, 0.999)), ' ',
+              unsafe_trunc(Int, 256 * clamp(g, 0.0, 0.999)), ' ',
+              unsafe_trunc(Int, 256 * clamp(b, 0.0, 0.999)), '\n')
 end
